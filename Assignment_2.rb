baa85a452dd3779e063c67c01087df273eabca8c
MalcolmD <madu7833@colorado.edu>
2013-09-22 16:27:52 -0600

    Assignment 2 - 9/22/2013

--- Assignment_2.rb
+++ Assignment_2.rb
@@ -70,6 +70,7 @@ P = Jellybean.new("black licorice", 50)
 #puts(P.get_name)
 #puts(P.get_calories)
 #puts(P.delicious?)
+#puts(P.healthy?)
 
 #Part 2a - OOP---------------------------
 
@@ -77,7 +78,7 @@ class Class
     def attr_accessor_with_history(attr_name)
       	 attr_name = attr_name.to_s          #make sure its a string
       	 attr_reader attr_name               #create the attribute's getter
-      	 attr_reader attr_name+"_history"  #create bar_history getter
+      	 attr_reader attr_name+"_history"    #create bar_history getter
        	 class_eval %Q{
 	  	   def #{attr_name}=(attr_name)
 		 	@attr_name = attr_name
@@ -101,5 +102,92 @@ end
 
 
 f = Foo.new
-f.bar = 3
-puts(f.bar_history)
+f.bar = 1
+f.bar = 2
+f.bar = 5
+f.bar = 6
+f.bar = 5
+f.bar = 8
+#print(f.bar_history) 
+#puts(f.bar)
+
+#Part 3a - OOP---------------------------
+class Numeric
+ 	
+      @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0}
+
+      def method_missing(method_id)
+	 singular_currency = method_id.to_s.gsub(/s$/, '')
+	 if @@currencies.has_key?(singular_currency)
+	    self / @@currencies[singular_currency]
+	 else
+	   super
+	 end
+      end
+
+
+      def dollar
+	return self 
+      end
+
+      def rupee
+	return self * @@currencies["rupee"]
+      end
+
+
+      def euro
+        return self * @@currencies["euro"]
+      end
+
+      def yen
+	return self * @@currencies["yen"]
+      end
+      
+      def in(currency)
+	method_missing(currency)
+      end
+
+
+end
+
+#puts(100.euro.in(:dollar))
+#puts(10.rupees.in(:euro))
+
+
+#Part 3b - String Palindromes---------------------------
+class String
+ def palindrome?()
+    l = self.downcase.gsub(/[^a-z]/, "")
+    r = l.reverse   
+   if(l == r)
+     puts "true"
+   else
+     puts "false"
+   end  
+ end
+end
+
+#puts("Mom".palindrome?())
+
+
+#Part 3c - Enumerables Palindromes-----------------------
+module Enumerable
+ def palindrome?()
+    l = self
+    r = []
+    l.reverse_each {|n| r << n }
+   if(l == r)
+     puts "true"
+   else
+     puts "false"
+   end  
+ end
+end
+
+puts([3, 3, 1, 1].palindrome?())
+
+
+#Part 4 - Blocks-----------------------------------------
+class CartesianProduct
+ include Enumberable
+
